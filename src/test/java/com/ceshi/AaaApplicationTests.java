848266ab12a9fe23204405e423eef0c4b835c7a8
shilb <1043318824@qq.com>
2020-08-11 21:00:45 +0800

    git aaa测试第一次提交

---
+++ src/test/java/com/ceshi/AaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
